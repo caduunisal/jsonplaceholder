74ff624a43c319ceeb5a90d470c3321555b8a5d3
caduunisal <kadu.unisal1@gmail.com>
2025-07-22 12:08:53 -0300

    Change Aapp.js with WSL IP Address

--- jsonplaceholder-app/frontend/src/App.js
+++ jsonplaceholder-app/frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
+
 function App() {
   const [usuarios, setUsuarios] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/usuarios')
+    axios.get(`${BACKEND_URL}/api/usuarios`)
       .then(res => setUsuarios(res.data))
       .catch(err => console.error('Erro na API:', err));
   }, []);
